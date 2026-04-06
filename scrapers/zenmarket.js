e035fa113843869056385437f7c67aa94729c608
Gus L.Z <glowrhorpeziehn@gmail.com>
2026-04-06 13:14:23 +0100

    fix zenmarket headers

--- scrapers/zenmarket.js
+++ scrapers/zenmarket.js
@@ -1,9 +1,33 @@
-const { fetchHTML } = require('./utils');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 const BASE = 'https://zenmarket.jp';
+
+async function fetchZenmarket(url) {
+  const response = await axios.get(url, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+      'Accept-Language': 'en-GB,en;q=0.9',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Referer': 'https://zenmarket.jp/en/',
+      'Origin': 'https://zenmarket.jp',
+      'Connection': 'keep-alive',
+      'Upgrade-Insecure-Requests': '1',
+      'Sec-Fetch-Dest': 'document',
+      'Sec-Fetch-Mode': 'navigate',
+      'Sec-Fetch-Site': 'same-origin',
+      'Cache-Control': 'max-age=0',
+    },
+    timeout: 20000,
+    maxRedirects: 5,
+  });
+  return cheerio.load(response.data);
+}
 
 module.exports = async function scrapeZenmarket({ query, page = 1 }) {
   const url = `${BASE}/en/mercari.aspx?q=${encodeURIComponent(query)}&p=${page}`;
-  const $ = await fetchHTML(url);
+  const $ = await fetchZenmarket(url);
   const listings = [];
 
   $('a.product-link').each((_, el) => {
